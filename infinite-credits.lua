80f6e4eab1a13b44dfe97ecfa4d9878aa5940029
Atypical <142782288+Attypical@users.noreply.github.com>
2023-09-11 15:20:10 +0300

    Create infinite-credits.lua

---
+++ infinite-credits.lua
@@ -0,0 +1,62 @@
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+	game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+
+if game.PlaceId == 648362523 then
+  player:Kick("not working")
+	repeat wait() until game:IsLoaded()
+	game.StarterGui:SetCore(
+	"SendNotification", {
+	Title = "Checking code...",
+	Text = "Here's a dog while you wait: 🐶",
+	Duration = 3
+})
+	wait(3)
+	game.StarterGui:SetCore(
+	"SendNotification", {
+	Title = "BP Inf Credits",
+	Text = "Made by Atypical (@attypical, discord)",
+	Duration = 10
+})
+
+	Clone = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:Clone()
+	game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:Destroy()
+	Clone.Parent = game:GetService("Players").LocalPlayer.Character
+	game.Players.LocalPlayer.settings["Display Gun"].Value = true
+	game.Players.LocalPlayer.CharacterAdded:Connect(
+		function()
+			if getgenv().AutoFarm then
+				pcall(function()
+					game.Players.LocalPlayer.settings["Display Gun"].Value = true
+					wait(4)
+					Clone = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:Clone()
+					game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:Destroy()
+					Clone.Parent = game:GetService("Players").LocalPlayer.Character
+				end)
+			end
+		end
+	)
+
+	while getgenv().AutoFarm do
+		pcall(function()
+			game:GetService("ReplicatedStorage").RemoteEvent:FireServer(16, "public")
+		end)
+		wait()
+		for i, v in pairs(game.Players:GetPlayers()) do
+			if v.Name == game.Players.LocalPlayer.Name then
+			else
+				pcall(function()
+					game:GetService("ReplicatedStorage").RemoteEvent:FireServer(30, v)
+				end)
+			end
+		end
+	end
+else
+	game:GetService("TeleportService"):Teleport(648362523, LocalPlayer)
+end
+
+
+if getgenv().ServerHop == true then
+	wait (1500)
+	game:GetService("TeleportService"):Teleport(648362523, LocalPlayer)
+end
