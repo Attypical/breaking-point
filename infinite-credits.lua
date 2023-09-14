d29e7909e70306f119c52ab7bdbd24541d9ec310
Atypical <142782288+Attypical@users.noreply.github.com>
2023-09-14 17:19:10 +0300

    Update infinite-credits.lua

--- infinite-credits.lua
+++ infinite-credits.lua
@@ -1,3 +1,8 @@
+local RS = game:GetService("ReplicatedStorage")
+if RS:FindFirstChild("ALREADYRUNNING") == nil then
+local part = instance.new("Part")
+part.Name = "ALREADYRUNNING"
+part.Parent = RS
 setclipboard("Infinite Credits script, made by the one and only Atypical. For contact, you can find me on discord @attypical")
 
 game:GetService("Players").LocalPlayer.Idled:connect(function()
@@ -52,4 +57,59 @@ end
 if getgenv().ServerHop == true then
 wait (1500)
 game:GetService("TeleportService"):Teleport(648362523, LocalPlayer)
+endsetclipboard("Infinite Credits script, made by the one and only Atypical. For contact, you can find me on discord @attypical")
+
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+
+if game.PlaceId == 648362523 then
+    repeat wait() until game:IsLoaded()
+     game.StarterGui:SetCore(
+        "SendNotification",
+        {
+            Title = "Warning! ⚠",
+            Text = "Only functions properly on the BP version made by Bluecage."
+        }
+      
+    Clone = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:Clone()
+    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:Destroy()
+    Clone.Parent = game:GetService("Players").LocalPlayer.Character
+    game.Players.LocalPlayer.settings["Display Gun"].Value = true
+    game.Players.LocalPlayer.CharacterAdded:Connect(
+        function()
+            if getgenv().ScriptEnabled then
+                pcall(function()
+                game.Players.LocalPlayer.settings["Display Gun"].Value = true
+                wait(4)
+                Clone = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:Clone()
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart:Destroy()
+                Clone.Parent = game:GetService("Players").LocalPlayer.Character
+                end)
+            end
+        end
+    )
+
+    while getgenv().ScriptEnabled do
+        pcall(function()
+        game:GetService("ReplicatedStorage").RemoteEvent:FireServer(16, "public")
+        end)
+        wait()
+        for i, v in pairs(game.Players:GetPlayers()) do
+            if v.Name == game.Players.LocalPlayer.Name then
+            else
+                pcall(function()
+                game:GetService("ReplicatedStorage").RemoteEvent:FireServer(30, v)
+                end)
+            end
+        end
+    end
+else
+    game:GetService("TeleportService"):Teleport(648362523, LocalPlayer)
+end
+
+if getgenv().ServerHop == true then
+wait (1500)
+game:GetService("TeleportService"):Teleport(648362523, LocalPlayer)
+end
 end
